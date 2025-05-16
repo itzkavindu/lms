5a18cfe973ea903d681229ef2187b47690493438
Kavindu Nimsara <kavibro000@gmail.com>
2025-05-17 00:19:43 +0530

    Delete server-old directory

--- server-old/configs/mongodb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-// Connect to MongoDB
-const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI}/LMS`);
-};
-
-export default connectDB;
